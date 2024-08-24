be652943d5f1add542ef2bb5e4b994c96512870b
Percival Li <p5li@uwaterloo.ca>
2024-08-24 07:29:48 -0400

    111

--- main/role.harvester.js
+++ main/role.harvester.js
@@ -9,15 +9,17 @@ var roleHarvester = {
             }
         }
         else {
-            if(Game.spawns['Spawn1'].store.getFreeCapacity() < 20)
-            {
-                var vacantExtention = _.filter(Game.structures, (Structure)=>Structure.structureType='extension' && (Structure.getFreeCapacity > 0))
-                if(creep.transfer(vacantExtention[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(vacantExtention[0]);
+            
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             }
-            else if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+            )
+
+            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0]);
             }
         }
 	}
