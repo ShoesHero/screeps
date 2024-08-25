1ec0ccf7a5dfb73845642f63a304e986f0a7c627
Percival Li <p5li@uwaterloo.ca>
2024-08-24 22:39:02 -0400

    111

--- main/main.js
+++ main/main.js
@@ -7,7 +7,7 @@ module.exports.loop = function () {
     var tower = Game.getObjectById('66ca030f0aebc4f822885b9f');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
+            filter: (structure) => structure.hits < 3500
         });
         if(closestDamagedStructure) {
             tower.repair(closestDamagedStructure);
