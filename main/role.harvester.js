5716b698043679b75722952f54beecb57bd09474
Percival Li <p5li@uwaterloo.ca>
2024-08-24 21:56:34 -0400

    111

--- main/role.harvester.js
+++ main/role.harvester.js
@@ -1,29 +1,35 @@
+const { filter } = require("lodash");
+
 var roleHarvester = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-	    if(creep.store.getFreeCapacity() > 0 && creep.memory.output == 0) {
+    run: function (creep) {
+        if (creep.store.getFreeCapacity() > 0 && creep.memory.output == 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
             }
         }
         else {
-            creep.memory.output=1;
-            
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+            creep.memory.output = 1;
+            if (creep.pos.findInRange(STRUCTURE_ROAD, 3, {
+                filter: function (object) {
+                    return object.hits < 3500;
                 }
+            }))
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                }
+                )
+            if (creep.transfer(targets[targets.length - 1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[targets.length - 1]);
             }
-            )
-            if(creep.transfer(targets[targets.length-1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[targets.length-1]);
-            }
-            if(creep.store.getUsedCapacity() == 0)
-            creep.memory.output=0;
+            if (creep.store.getUsedCapacity() == 0)
+                creep.memory.output = 0;
         }
-	}
+    }
 };
 module.exports = roleHarvester;
