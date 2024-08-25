015914c2e917ef0c1a5a8652e6d75d69f25ccfbf
Percival Li <p5li@uwaterloo.ca>
2024-08-24 22:27:43 -0400

    111

--- main/role.upgrader.js
+++ main/role.upgrader.js
@@ -4,17 +4,25 @@ var roleUpgrader = {
     run: function (creep) {
         if (creep.store.getFreeCapacity() > 0 && creep.memory.output == 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            if (creep.memory.role == 'harvester') {
+                if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[1]);
+                }
             }
+            else {
+                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+                }
+            }
+
         }
         else {
-            creep.memory.output=1;
+            creep.memory.output = 1;
             if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
-            if(creep.store.getUsedCapacity() == 0)
-                creep.memory.output=0;
+            if (creep.store.getUsedCapacity() == 0)
+                creep.memory.output = 0;
         }
     }
 };
