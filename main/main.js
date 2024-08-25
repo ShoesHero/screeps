9be782453f04ef60b6a3791bc9e747e9d075f77b
Percival Li <p5li@uwaterloo.ca>
2024-08-25 01:07:01 -0400

    111

--- main/main.js
+++ main/main.js
@@ -1,5 +1,6 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleUpgrader2 = require('role.upgrader2');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
@@ -26,25 +27,27 @@ module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var upgraders2 = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader2');
 
     if(harvesters.length < 4)
     Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE],'harvester'+Math.floor(Math.random() * 10000),{memory:{role:'harvester'}})
-    if(upgraders.length < 4)
+    else if(upgraders.length < 4)
     Game.spawns['Spawn1'].spawnCreep([WORK,WORK,WORK,WORK,CARRY,MOVE],'upgrader'+Math.floor(Math.random() * 10000),{memory:{role:'upgrader'}})
-    if(Game.spawns['Spawn1'].pos.findClosestByRange(FIND_CONSTRUCTION_SITES) != null && builders.length < 2)
-    Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,CARRY,CARRY,MOVE],'upgrader'+Math.floor(Math.random() * 10000),{memory:{role:'builder'}})
-
+    else if(Game.spawns['Spawn1'].pos.findClosestByRange(FIND_CONSTRUCTION_SITES) != null && builders.length < 2)
+    Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,CARRY,CARRY,MOVE],'builder'+Math.floor(Math.random() * 10000),{memory:{role:'builder'}})
+    else if(upgraders2.length < 2)
+    Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,CARRY,CARRY,MOVE],'2upgrader'+Math.floor(Math.random() * 10000),{memory:{role:'upgrader2'}})
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
-            if(Game.spawns['Spawn1'].store.getFreeCapacity() == 0)
-            roleUpgrader.run(creep);
-        else
             roleHarvester.run(creep);
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'upgrader2') {
+            roleUpgrader2.run(creep);
         }
         if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
