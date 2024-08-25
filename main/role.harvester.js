d4c11ad4543e3115c0e4d80ee049fe64c544d6b4
Percival Li <p5li@uwaterloo.ca>
2024-08-25 03:28:20 -0400

    111

--- main/role.harvester.js
+++ main/role.harvester.js
@@ -12,24 +12,24 @@ var roleHarvester = {
         }
         else {
             creep.memory.output = 1;
-            if (creep.pos.findInRange(STRUCTURE_ROAD, 1, {
-                filter: function (object) {
-                    return object.hits < 3500;
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
-            }))
-            creep.repair(creep.pos);
-                var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-                }
-                )
-            if (creep.transfer(targets[targets.length - 1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[targets.length - 1]);
             }
-            if (creep.store.getUsedCapacity() == 0)
-                creep.memory.output = 0;
+            )
+            if (targets == null) {
+                CREEP_LIFE_TIME.moveTo(38, 20);
+            }
+            else {
+                if (creep.transfer(targets[targets.length - 1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[targets.length - 1]);
+                }
+                if (creep.store.getUsedCapacity() == 0)
+                    creep.memory.output = 0;
+            }
+
         }
     }
 };
