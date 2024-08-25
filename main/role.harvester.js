7fa49de0115f7a480f7dd718c66acf14fd99f822
Percival Li <p5li@uwaterloo.ca>
2024-08-24 22:04:02 -0400

    111

--- main/role.harvester.js
+++ main/role.harvester.js
@@ -20,7 +20,7 @@ var roleHarvester = {
             creep.repair(creep.pos);
                 var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
                 }
