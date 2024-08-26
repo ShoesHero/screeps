d64b19cf9c03df43d726623bae5d97890486a894
Percival Li <p5li@uwaterloo.ca>
2024-08-25 20:14:57 -0400

    111

--- main/main.js
+++ main/main.js
@@ -8,7 +8,7 @@ module.exports.loop = function () {
     var tower = Game.getObjectById('66ca030f0aebc4f822885b9f');
     if (tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < 3500 && structure.structureType == STRUCTURE_ROAD
+            filter: (structure) => structure.hits / structure.hitsMax < 0.5 && (structure.structureType != STRUCTURE_ROAD)
         });
         if (closestDamagedStructure) {
             tower.repair(closestDamagedStructure);
@@ -30,13 +30,13 @@ module.exports.loop = function () {
     var upgraders2 = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader2');
 
     if (harvesters.length < 4)
-        Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], 'harvester' + Math.floor(Math.random() * 10000), { memory: { role: 'harvester' } })
+        Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], 'harvester' + Math.floor(Math.random() * 10000), { memory: { role: 'harvester' } })
     else if (upgraders.length < 4)
         Game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, WORK, CARRY, MOVE], 'upgrader' + Math.floor(Math.random() * 10000), { memory: { role: 'upgrader' } })
     else if (Game.spawns['Spawn1'].pos.findClosestByRange(FIND_CONSTRUCTION_SITES) != null && builders.length < 2)
         Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE], 'builder' + Math.floor(Math.random() * 10000), { memory: { role: 'builder' } })
-    else if (upgraders2.length < 2)
-        Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE], '2upgrader' + Math.floor(Math.random() * 10000), { memory: { role: 'upgrader2' } })
+    else if (upgraders2.length < 4)
+        Game.spawns['Spawn1'].spawnCreep([WORK, WORK,WORK,WORK, CARRY, CARRY, CARRY, MOVE,MOVE], '2upgrader' + Math.floor(Math.random() * 10000), { memory: { role: 'upgrader2' } })
 
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
