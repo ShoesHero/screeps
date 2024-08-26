960ae856a87ffb89e8c67166a25f867ab0a5de94
Percival Li <p5li@uwaterloo.ca>
2024-08-27 00:33:30 +0800

    111

--- main/role.salvager.js
+++ main/role.salvager.js
@@ -9,9 +9,22 @@ var role = {
             }
             console.log('开始拾荒');
         }
+
+        if (creep.store.getUsedCapacity() > 0) {
+
+                var storage = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_STORAGE &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+                    }
+                })
+                if (creep.transfer(storage[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(storage[0]);
+                }
+            }
+        }
     }
 
-}
 module.exports = role;
 
 
