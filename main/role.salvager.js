860f540a9d13cdfff13723984142cfb6decdabb8
Percival Li <p5li@uwaterloo.ca>
2024-08-27 00:29:02 +0800

    111

--- main/role.salvager.js
+++ main/role.salvager.js
@@ -4,7 +4,7 @@ var role = {
         var drope = creep.room.find(FIND_SOURCES);
 
         if (drope.length > 0 && creep.room.name == 'W49N3') {
-            if (creep.withdraw(drope[0], RESOURCE_ALL) == ERR_NOT_IN_RANGE) {  // 指定资源类型
+            if (creep.withdraw(drope[0], RESOURCES_ALL) == ERR_NOT_IN_RANGE) {  // 指定资源类型
                 creep.moveTo(drope[0], { visualizePathStyle: { stroke: '#ffaa00' } });
             }
             console.log('开始拾荒');
