19f7efd068242bca252f42ba44b89b341bb44196
Percival Li <p5li@uwaterloo.ca>
2024-08-26 04:31:52 -0400

    111

--- main/role.harvester.js
+++ main/role.harvester.js
@@ -4,7 +4,7 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        if (creep.store.getFreeCapacity() > 0 && creep.memory.output == 0) {
+        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0 && creep.memory.output == 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
