8948d6fc730c36bb97999f4f001cf675ec1f316d
Percival Li <p5li@uwaterloo.ca>
2024-08-29 20:15:54 +0800

    111

--- main/role.melee.js
+++ main/role.melee.js
@@ -1,13 +1,13 @@
 var roleMelee = {
     run: function (creep) {
-        if (creep.room.name == 'W49N2') {
-            var enemy = creep.room.find(FIND_HOSTILE_CREEPS);
+        if (creep.room.name == 'W46N3') {
+            var enemy = creep.room.find(FIND_HOSTILE_STRUCTURES, {filter: { structureType: STRUCTURE_SPAWN }});
             if (creep.attack(enemy[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(enemy[0]);
             }
         }
         else {
-            creep.moveTo(new RoomPosition(30, 2, 'W49N2'), { visualizePathStyle: { stroke: '#ffaa00' } })
+            creep.moveTo(new RoomPosition(30, 2, 'W46N3'), { visualizePathStyle: { stroke: '#ffaa00' } })
         }
     }
 }
