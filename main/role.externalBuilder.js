44edac368a2f4ea0c50e2ca4b0593b847e9bed0d
Percival Li <p5li@uwaterloo.ca>
2024-09-01 21:14:39 +0800

    111

--- main/role.externalBuilder.js
+++ main/role.externalBuilder.js
@@ -6,30 +6,24 @@ var roleExternalBuilder = {
             creep.moveTo(new RoomPosition(30, 49, 'W46N3'), { visualizePathStyle: { stroke: '#ffaa00' } })
         else {
             if (creep.store.getFreeCapacity() > 0 && creep.memory.output == 0) {
-                if (creep.room.name == 'W49N3') {
-                    var sources = creep.room.find(FIND_SOURCES);
-                    if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(sources[1]);
-                    }
-                }
-                else {
-                    creep.memory.output = 1;
 
-                    const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-                    if (creep.build(target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target);
-                    }
-                    if (creep.store.getUsedCapacity() == 0)
-                        creep.memory.output = 0;
+                var sources = creep.room.find(FIND_SOURCES);
+                if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[1]);
                 }
+            }
+            else {
+                creep.memory.output = 1;
 
+                const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+                if (creep.build(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+                if (creep.store.getUsedCapacity() == 0)
+                    creep.memory.output = 0;
             }
         }
 
     }
-
-
 }
-
-
 module.exports = roleExternalBuilder
